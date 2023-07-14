273bf479e30f35dad7dd239082797fe6458659eb
ThanhSon <nguyenthanhsont23@gmail.com>
2023-07-14 20:07:08 +0700

    layout

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\GroupController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [HomeController::class,'index']);
+Route::get('/group', [GroupController::class,'index']);
+Route::get('/son', [HomeController::class,'test']);
